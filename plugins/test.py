9574bfaab502f6f9f8857ce351495707a539b0aa
Jonas Schulze <jonas.schulze@st.ovgu.de>
2018-02-12 20:58:32 +0100

    Update reply

--- plugins/test.py
+++ plugins/test.py
@@ -9,5 +9,5 @@ import time
 
 @respond_to('^test$', re.IGNORECASE)
 def test(message):
-    message.reply("Hi there", in_thread=True)
+    message.reply(message['thread_ts'] or 'None')
     message.react('+1')
