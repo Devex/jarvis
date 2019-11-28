995a79f6073cc6664743c159dfe2c1e6f5c2edae
Ignasi Fosch <ignasi.fosch@devex.com>
2019-11-28 10:44:43 +0100

    Enable use of `DEBUG` envvar

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,5 +1,6 @@
 import os
 
+DEBUG = os.environ.get('DEBUG', False)
 API_TOKEN = os.environ['SLACKBOT_API_TOKEN'] or ''
 DEFAULT_REPLY = "I don't understand this reference, sir."
 ERRORS_TO = os.environ['SLACKBOT_ERRORS_DEST'] or None
