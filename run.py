318bcdd5bfa40a78ca9162479fcb6b54b815bc66
Ignasi Fosch <ignasi.fosch@devex.com>
2019-11-28 10:28:19 +0100

    Enable logging for slackbot

--- run.py
+++ run.py
@@ -1,8 +1,22 @@
+import logging
+import sys
+from slackbot import settings
 from slackbot.bot import Bot
 
+
 def main():
+    kw = {
+        'format': '[%(asctime)s] %(name)s: %(message)s',
+        'datefmt': '%m/%d/%Y %H:%M:%S',
+        'level': logging.DEBUG if settings.DEBUG else logging.INFO,
+        'stream': sys.stdout,
+    }
+    logging.basicConfig(**kw)
+    logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(
+        logging.WARNING)
     bot = Bot()
     bot.run()
+
 
 if __name__ == "__main__":
     main()
